ac869a11c42f61113bea603f91a2c9c58b28b8b7
Aleqyan666 <haykalekyan@gmail.com>
2024-08-26 10:30:20 +0400

    activate_prefect.bat comments added

--- prefect_activation/activate_prefect.bat
+++ prefect_activation/activate_prefect.bat
@@ -1,5 +1,13 @@
 @echo off
+REM Navigate to the 'Scripts' directory inside your virtual environment.
+REM Make sure the path to '.venv\Scripts' matches your virtual environment's location.
 cd .venv\Scripts 
+
+REM Activate the virtual environment.
 call Activate
+
+REM Move back to the project root directory.
 cd ../..
+
+REM Start the Prefect server.
 prefect server start
