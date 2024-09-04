046f15188391a62813ea39c975a67e2821eac47c
Aleqyan666 <haykalekyan@gmail.com>
2024-09-04 17:50:16 +0400

    Prefect fully automated

--- latest_prefect/config_deploy.bat
+++ latest_prefect/config_deploy.bat
@@ -1,15 +1,24 @@
 @echo off
 REM Activate the virtual environment.
 call .venv\Scripts\Activate
-REM Prompting the user for input
-set  DEPLOYMENT_NAME=%1
-set  FILE_NAME=%2
-set  FLOW_NAME=%3
 
-prefect deploy -n "%DEPLOYMENT_NAME%" "%FILE_NAME%.py:%FLOW_NAME%"
+REM Prompting the user for input or use default values
+set DEPLOYMENT_NAME=%1
+set FILE_NAME=%2
+set FLOW_NAME=%3
+set CRON_SCHEDULE=%4
+set TIMEZONE=%5
+set WORK_POOL=%6
 
-REM Starting the Prefect Agent
-prefect worker start -q "default"
+REM Deploy the flow with the provided parameters and automatically answer prompts
+(
+    echo n
+    echo n
+) | prefect deploy -n "%DEPLOYMENT_NAME%" "%FILE_NAME%.py:%FLOW_NAME%" --cron %CRON_SCHEDULE% --timezone %TIMEZONE% --pool %WORK_POOL%
+
+REM Starting the Prefect Worker with the specified work pool
+prefect worker start --pool "%WORK_POOL%" 
+
 @echo
 @echo
 @echo
--- latest_prefect/deploy.py
+++ latest_prefect/deploy.py
@@ -3,13 +3,16 @@ import subprocess
 # Path to batch script
 script_path = "latest_prefect\\config_deploy.bat"
 
-DEPLOYMENT_NAME = 'HaykIsTesting'
+DEPLOYMENT_NAME = 'qquoteschanged'
 FILE_NAME = 'csv_util'
 FLOW_NAME = 'csv_manipulation_flow'
+CRON_SCHEDULE = '2 2 * * 2'
+TIMEZONE = 'Asia/Yerevan'
+WORK_POOL = 'first_pool'
 
 try:
     # Run the batch script
-    subprocess.run([script_path,DEPLOYMENT_NAME,FILE_NAME,FLOW_NAME], 
+    subprocess.run([script_path, DEPLOYMENT_NAME, FILE_NAME, FLOW_NAME, CRON_SCHEDULE, TIMEZONE, WORK_POOL], 
                    shell=True, check=True)
 
 except subprocess.CalledProcessError as e:
